505a6128833549c556efa02ad52d74531c7b62a2
yykdev <assavictory@gmail.com>
2020-07-28 15:10:02 +0900

    My Work 필터 기능 및 애니메이션 효과 적용

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,14 +30,54 @@ homeContactBtn.addEventListener("click", () => {
     scrollIntoView("#contact");
 });
 
-function scrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-}
-
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show 'arrow up' button when scrolling down
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add("visible");
+    } else {
+        arrowUp.classList.remove("visible");
+    }
+});
+
+// handle click on the 'arrow up' button
+arrowUp.addEventListener("click", () => {
+    scrollIntoView("#home");
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
+
+// Project
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+    const filter =
+        e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add("anim-out");
+
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+
+        projectContainer.classList.remove("anim-out");
+    }, 300);
 });
